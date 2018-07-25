16ad7b2f6f2b51e88af08ec018c78b021dcbaf23
mark <snowtowind@yahoo.com>
2018-07-24 18:36:57 -0700

    rewrote function and chg bkgd color

--- part_System.js
+++ part_System.js
@@ -5,7 +5,7 @@ function setup() {
   // put setup code here
   can = createCanvas(800, 500);
   can.position(100, 50);can.class('can');
-  can.mousePressed(on_of);ptitle=createP("A Fireworks Show");
+  can.mousePressed(()=>{if(count%2==0){noLoop();}  else{loop();}count++;});ptitle=createP("A Fireworks Show");
   ptitle.class('p');ptitle.position(450,-15);
 stroke(250,0,0);
 strokeWeight(5);
@@ -16,9 +16,10 @@ fireworks.push(new particles(random(width),height,0,random(-25)));
 
 function draw() {
   r=random(10);
-  background(0,200,0);
+  //background(0,200,0);
 if(r<2){
-fireworks.push(new particles(random(width),height,0,random(-12,-5)));r=random(10);}
+fireworks.push(new particles(random(width),height,0,random(-12,-5)));r=random(10);
+background(random(250),random(250),0);}
   for(let j=0;j<fireworks.length;j++){
     fireworks[j].update();
   fireworks[j].show();
@@ -26,4 +27,4 @@ fireworks.push(new particles(random(width),height,0,random(-12,-5)));r=random(10
 r=random(10);
 }
 
-function on_of(){if(count%2==0){noLoop();}  else{loop();}count++;}
+//function on_of(){if(count%2==0){noLoop();}  else{loop();}count++;}
