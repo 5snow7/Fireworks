3d4c24fb56d33ac7c16aa15ad24f0615f26f2302
PhsTigers <mwomack@pcoe.k12.ca.us>
2019-03-17 08:47:45 -0700

    chg bckgrd

--- part_System.js
+++ part_System.js
@@ -15,7 +15,7 @@ fireworks.push(new particles(random(width),height,0,random(-25)));
 
 function draw() {
   r=random(10);
-  background(0,200,0);
+  background(50,200,0);
 if(r<2){
 fireworks.push(new particles(random(width),height,0,random(-12,-5)));r=random(10);}
   for(let j=0;j<fireworks.length;j++){
